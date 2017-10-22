a79010a0dcc8b50f4f3a7aa400b57095c2d3b2e5
tonymichaelhead <tonymichaelhead@gmail.com>
2017-10-21 18:41:28 -0700

    Configure get root route

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/../client')));
 
 app.get('/', function(req, res) {
-    
+
         // 
         res.render('../client/index.js');
     });
