d5ca574829b7c1448d9697cb25a0a2324e4fd334
tonymichaelhead <tonymichaelhead@gmail.com>
2017-10-21 19:08:31 -0700

    Take app.get root out of server routes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 // Server setup
 const port = process.env.PORT || 3000;
-app.set('port', port);
+// app.set('port', port);
 
 // Middleware
 app.use(bodyParser.json());
@@ -14,10 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, '../client')));
 
-app.get('/', function(req, res) {
-        // 
-    res.sendFile(path.join(__dirname, '../client/index.html'));
-});
+// app.get('/', function(req, res) {
+//         // 
+//     res.sendFile(path.join(__dirname, '../client/index.html'));
+// });
 
 app.listen(port, function () {
     console.log(`Listening on port ${port}`);
